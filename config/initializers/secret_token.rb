cf0ef21feb43d11fdeac99194eaaf8795d903356
James Dunwoody <jamesd@hooroo.com>
2013-07-09 14:47:22 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShiftySearch::Application.config.secret_key_base = '64cb58668ddb0739a21bdd331236192b52a28d3380cb7f06bebd43b256cd377925990a6a9e5012eccea45ad32b1ced9a0c7e468a6243ae8ee0cee076bea914c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShiftySearch::Application.config.session_store :cookie_store, key: '_shifty_search_session'
